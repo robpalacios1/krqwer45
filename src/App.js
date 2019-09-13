888196f107b0d4ae0dff6be31d5688d02df52cc7
Roberto <robpalacios1@hotmail.com>
2019-09-13 17:40:33 -0500

    modify component to add task and cross out

--- src/App.js
+++ src/App.js
@@ -3,87 +3,89 @@ import React from 'react';
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
+      newTask: '',
       tasks: [
         { id: 1, name: "Sacar la ropa", done: false },
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: '',
-      isInValid: false
+      errors: {
+        newTask: false
+      }
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.inputchange = this.inputchange.bind(this)
-    this.changeTaskState = this.changeTaskState.bind(this)
   }
-  handleSubmit (event){
-    if (this.state.newTask === ''){
+  validateNewTask() {
+    if (this.state.newTask === '') {
       this.setState({
-        isInValid: true
+        errors: {
+          newTask: true
+        }
       })
-    }else{
-      const oldTasks = this.state.tasks
-      const last = oldTasks.slice(-1)[0]
-      const newTask = {
-        id: last.id + 1,
+      return false
+    }
+    return true
+  }
+  addTask(event) {
+    event.preventDefault()
+    if (this.validateNewTask()) {
+      let oldTasks = this.state.tasks
+      let newTask = {
+        id: Math.max(...oldTasks.map(task => task.id)) + 1,
         name: this.state.newTask,
         done: false
       }
       this.setState({
         tasks: [...oldTasks, newTask],
-        newTask: '',
-        isInValid: false
+        newTask: ''
       })
     }
-    event.preventDefault()
   }
-
-  inputchange (event){
+  updateTask(event) {
     this.setState({
-      newTask: event.target.value
+      newTask: event.target.value,
+      errors: {
+        newTask: false
+      }
     })
   }
-
-  changeTaskState (id){
-    const newArr = this.state.tasks.map(task => {
-      if (task.id === id){
+  toogleDone(id,event) {
+    const newTasks = this.state.tasks.map(task => {
+      if (task.id === id) {
         task.done = !task.done
+        return task
       }
       return task
     })
     this.setState({
-      tasks: newArr
+      tasks: newTasks
     })
   }
-
-  render() {
+  renderTasks() {
+    const tasks = this.state.tasks.map((task, index) => {
+      return (
+        <li
+          className={task.done ? 'done' : null}
+          key={task.id}
+          onClick={this.toogleDone.bind(this, task.id)}>
+          {task.name}
+        </li>
+      )
+    })
+    return tasks
+  }
+   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task) => {
-              return(
-                <li
-                  className = {task.done ? 'done' : ''}
-                  key={task.id}
-                  onClick = {() => this.changeTaskState(task.id)}>
-                  {task.name}
-                </li>
-              )
-            })}
+            {this.renderTasks()}
           </ul>
-          <form onSubmit = {this.handleSubmit}>
-            <input
-            type="text"
-            id="new-task"
-            placeholder="Ingresa una tarea y oprime Enter"
-            value={this.state.newTask}
-            onChange={this.inputchange}
-            className= {this.state.isInValid ? 'error' : ''}
-          />
+          <form onSubmit={this.addTask.bind(this)}>
+          <input className={this.state.errors.newTask ? 'error' : null} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.updateTask.bind(this)}/>
           </form>
         </div>
       </div>
